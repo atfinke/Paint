99ae2c9b0db5ef50866e31330ae166e681fef0e3
Andrew Finke <andrewfinke@me.com>
2017-10-15 16:42:00 -0500

    Init project

---
+++ Paint/Paint macOS/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Paint macOS
+//
+//  Created by Andrew Finke on 10/15/17.
+//  Copyright © 2017 Andrew Finke. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
